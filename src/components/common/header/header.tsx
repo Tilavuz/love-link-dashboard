ce08e0e460e35d872df687fd548901023e6fc8ab
Tilavuz <tilav777@gamil.com>
2025-02-08 23:41:57 +0500

    Soem changed

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -3,9 +3,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/app/store";
 import { serverUrl } from "@/helpers/shared";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useNavigate } from "react-router-dom";
 
 export default function Header({ children }: { children: ReactNode }) {
   const { auth } = useSelector((state: RootState) => state.auth);
+
+  const navigate = useNavigate();
 
   return (
     <header className="w-full mt-2 p-2 border-b">
@@ -14,13 +17,18 @@ export default function Header({ children }: { children: ReactNode }) {
           {children}
           <div></div>
         </div>
-        <div className="flex items-center gap-2 cursor-pointer border-l pl-4 w-[210px]">
+        <div
+          onClick={() => navigate("/profile")}
+          className="flex items-center gap-2 cursor-pointer border-l pl-4 w-[210px]"
+        >
           <Avatar className="w-14 h-14">
             <AvatarImage
               src={`${serverUrl}/uploads/${auth?.photo}`}
               alt={auth?.name}
             />
-            <AvatarFallback className="uppercase">{auth?.username?.charAt(0)}</AvatarFallback>
+            <AvatarFallback className="uppercase">
+              {auth?.username?.charAt(0)}
+            </AvatarFallback>
           </Avatar>
           <div>
             <p className="font-bold">{auth?.username}</p>
